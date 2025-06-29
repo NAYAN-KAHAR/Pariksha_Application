623bf55749c66ad5542ae2637966afb34cf99845
NAYAN-KAHAR <kaharnayan99@gmail.com>
2025-06-29 18:41:08 +0530

    url fixed

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://jovial-axolotl-4615e1.netlify.app',
   credentials: true,
 }));
 
